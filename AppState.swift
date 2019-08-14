48beabfd25a3d34be081f29fa3c89c116e70d5cd
Arthur Fanck <arthur.fanck@gmail.com>
2019-08-14 18:46:47 -0300

    Added some functions

---
+++ AppState.swift
@@ -0,0 +1,13 @@
+//
+//  AppState.swift
+//  ReSwiftNanoChallenge
+//
+//  Created by Arthur Bastos Fanck on 14/08/19.
+//  Copyright © 2019 Raul Rodrigues. All rights reserved.
+//
+
+import ReSwift
+
+struct AppState: StateType {
+    
+}
